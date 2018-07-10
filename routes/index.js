c8339c4ae35931aaecc426a2b902c10a73f7bc2e
Eric Frankel <eric-frankel@cox.net>
2018-07-09 17:47:45 -0700

    updated index and installed another package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const url = "http://api.giphy.com/v1/gifs/random?api_key=2WPXFgfmjh0s7kMycJ7HJtoBdQq7ed2F";
+  request.get(url, (err, response, body) => {
+    if (err) {
+      console.error(err);
+    }
+    body = JSON.parse(body);
+    console.log(body);
+
+    res.render('index', { title: 'Express' });
+  });
 });
 
 module.exports = router;
