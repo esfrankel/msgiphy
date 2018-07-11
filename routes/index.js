058c3fe97d22965ac51b953fc6f2f0bdf6de1f5a
Eric Frankel <eric-frankel@cox.net>
2018-07-11 02:02:12 -0700

    adding more routes for giphy

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var router = express.Router();
-const request = require('request');
+const express = require('express');
+const router = express.Router();
 
+// Add this line
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const url = "http://api.giphy.com/v1/gifs/random?api_key=2WPXFgfmjh0s7kMycJ7HJtoBdQq7ed2F";
+
+  // Add the following 5 lines (be sure to add _your_ API key to the URL)
+  const url = "http://api.giphy.com/v1/gifs/random?api_key=YOUR-API-KEY";
   request.get(url, (err, response, body) => {
-    if (err) {
-      console.error(err);
-    }
+    if(err) { console.error(err) }
+
     body = JSON.parse(body);
     console.log(body);
 
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Make School Giphy' });
   });
 });
 
