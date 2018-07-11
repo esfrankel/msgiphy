520412ca0476b5211637f2e9cae6907eb926a256
Eric Frankel <eric-frankel@cox.net>
2018-07-11 02:10:10 -0700

    fixed so display random image

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
-// Add this line
 const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  // Add the following 5 lines (be sure to add _your_ API key to the URL)
-  const url = "http://api.giphy.com/v1/gifs/random?api_key=YOUR-API-KEY";
+  const url = "http://api.giphy.com/v1/gifs/random?api_key=2WPXFgfmjh0s7kMycJ7HJtoBdQq7ed2F";
   request.get(url, (err, response, body) => {
     if(err) { console.error(err) }
 
     body = JSON.parse(body);
-    console.log(body);
 
-    res.render('index', { title: 'Make School Giphy' });
+    // Add this line to get the .gif's URL from the Giphy response body:
+    const imgUrl = body.data.image_url
+
+    // And pass it to our view as imgUrl:
+    res.render('index', { title: 'Make School Giphy', imgUrl: imgUrl });
   });
 });
 
